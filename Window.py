c243d92865db63f869b96d5a73ebdeeb63fec542
Nissaar <34180849+Nissaar@users.noreply.github.com>
2019-10-14 23:52:22 +0400

    Update Window.py

--- Window.py
+++ Window.py
@@ -2,32 +2,32 @@ import sys
 import os
 from tkinter import *
 
-def EntryForm():
-    os.system('py C:/Users/username/Desktop/Python/Project/entryform.py')
+def Create():
+    os.system('py C:/Users/nisgo/Desktop/Python/Project/Create.py')
 
-def ViewDB():
-    os.system('py C:/Users/username/Desktop/Python/Project/viewDB.py')
+def Read():
+    os.system('py C:/Users/nisgo/Desktop/Python/Project/Read.py')
 
-def EditDB():
-    os.system('py C:/Users/username/Desktop/Python/Project/Edit.py')
+def Update():
+    os.system('py C:/Users/nisgo/Desktop/Python/Project/Update.py')
 
 def DeleteRow():
-    os.system('py C:/Users/username/Desktop/Python/Project/Delete.py')
+    os.system('py C:/Users/nisgo/Desktop/Python/Project/Delete.py')
 
 window = Tk()
 window.title("PyDB App")
 window.geometry("400x500")
 
-insertButton = Button(window, height = "15", width = "25", text = "Entry form", background = "white", foreground = "red", command = EntryForm)
+insertButton = Button(window, height = "15", width = "25", text = "Create", background = "white", foreground = "red", command = Create)
 insertButton.place(x=10, y=10)
 
-readButton = Button(window, height = "15", width = "25", text = "View DB", background = "white", foreground = "red", command = ViewDB)
+readButton = Button(window, height = "15", width = "25", text = "Read", background = "white", foreground = "red", command = Read)
 readButton.place(x=200, y=10)
 
-updateButton = Button(window, height = "15", width = "25", text = "Edit", background = "white", foreground = "red", command = EditDB)
+updateButton = Button(window, height = "15", width = "25", text = "Update", background = "white", foreground = "red", command = Update)
 updateButton.place(x=10, y=250)
 
-deleteButton = Button(window, height = "15", width = "25", text = "Delete", background = "white", foreground = "red", command = DeleteRow)
+deleteButton = Button(window, height = "15", width = "25", text = "Delete", background = "white", foreground = "red", command = Delete)
 deleteButton.place(x=200, y=250)
 
 window.mainloop()
