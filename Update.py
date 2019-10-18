273449bf37944824b45111b46e3a6cf947961345
Nissaar <34180849+Nissaar@users.noreply.github.com>
2019-10-19 00:20:15 +0400

    Update Update.py

--- Update.py
+++ Update.py
@@ -38,13 +38,13 @@ twoframe.grid(column=0,row=3)
 threeframe = Frame(window)
 threeframe.grid(column=0, row=4)
 
-namelabel = Label(twoframe, text = "Name")
+namelabel = Label(twoframe, text = "Name", width=8, bg="grey")
 namelabel.pack(side = "left")
 
 nameEntry = Entry(twoframe, background = "white", justify = "center")
 nameEntry.pack(side= "right")
 
-Bountylabel = Label(threeframe, text = "Bounty")
+Bountylabel = Label(threeframe, text = "Bounty", width=8, bg="grey")
 Bountylabel.pack(side = "left")
 
 BountyEntry = Entry(threeframe, background = "white", justify = "center")
@@ -60,21 +60,21 @@ def fetchfromdb():
         nameEntry.insert(0, val[1])
         BountyEntry.insert(0, val[2])
 
-IDlabel = Label(oneframe, text = "ID")
+IDlabel = Label(oneframe, text = "ID", width=8, bg="grey")
 IDlabel.pack(side = "left")
 
 IDEntry = Entry(oneframe, background = "white", justify = "center")
 IDEntry.pack(side= "right")
 
-fetchButton = Button(window, width = "18", background = "white", foreground = "red", text = "Fetch", command = fetchfromdb)
-fetchButton.grid(column=0, row=2)
+fetchButton = Button(window, width = "26", background = "white", foreground = "red", text = "Fetch", command = fetchfromdb)
+fetchButton.grid(column=0, row=2, padx=33)
 
 
-UpdateButton = Button(window, width = "18", background = "white", foreground = "red", text = "Update", command = Update)
-UpdateButton.grid(column=0, row=5)
+UpdateButton = Button(window, width = "26", background = "white", foreground = "red", text = "Update", command = Update)
+UpdateButton.grid(column=0, row=5, padx=33)
 
-cancelButton = Button(window, width = "18", background = "white", foreground = "red", text = "Cancel", command = Cancel)
-cancelButton.grid(column=0, row=7)
+cancelButton = Button(window, width = "26", background = "white", foreground = "red", text = "Cancel", command = Cancel)
+cancelButton.grid(column=0, row=7, padx=33)
 
 window.mainloop()
 
